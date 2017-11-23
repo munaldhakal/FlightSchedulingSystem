219f7390cacfa19fe8486475ff0a01bdc1d8ada1
Munal <dhakal.munal@gmail.com>
2017-11-23 22:51:38 +0545

    Register User Finished and Databases created

--- src/com/flightscheduler/JavaEE/dto/RegisterUserDto.java
+++ src/com/flightscheduler/JavaEE/dto/RegisterUserDto.java
@@ -2,4 +2,74 @@ package com.flightscheduler.JavaEE.dto;
 
 public class RegisterUserDto {
 
+	private String userType;
+	private String userName;
+	private String password;
+	private String email;
+	private String phoneNumber;
+	private Long id;
+
+	public RegisterUserDto() {
+		super();
+	}
+
+	public RegisterUserDto(String userType, String userName, String password, String email, String phoneNumber,
+			Long id) {
+		super();
+		this.userType = userType;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.id = id;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 }
--- src/com/flightscheduler/JavaEE/servlet/RegisterServlet.java
+++ src/com/flightscheduler/JavaEE/servlet/RegisterServlet.java
@@ -1,40 +1,46 @@
 package com.flightscheduler.JavaEE.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class RegisterServlet
- */
+import com.flightscheduler.JavaEE.database.SaveUserInfo;
+import com.flightscheduler.JavaEE.dto.RegisterUserDto;
+import com.flightscheduler.JavaEE.model.RegisterUser;
+
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	RegisterUser registerUser = new RegisterUser();
+	RegisterUserDto registerUserDto = new RegisterUserDto();
+
+	public RegisterServlet() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		registerUserDto.setUserType(request.getParameter("userType"));
+		registerUser.setUserType(registerUserDto.getUserType());
+		registerUserDto.setEmail(request.getParameter("email"));
+		registerUser.setEmail(registerUserDto.getEmail());
+		registerUserDto.setUserName(request.getParameter("username"));
+		registerUser.setUserName(registerUserDto.getUserName());
+		registerUserDto.setPassword(request.getParameter("password"));
+		registerUser.setPassword(registerUserDto.getPassword());
+		registerUserDto.setPhoneNumber(request.getParameter("phoneNumber"));
+		registerUser.setPhoneNumber(registerUserDto.getPhoneNumber());
+		SaveUserInfo.saveUser(registerUser);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
