09035e1bcb6c349ec5574cb2353acc4f4baa55db
sauravsitoula <saaurav10@gmail.com>
2017-11-26 19:11:15 +0545

    TravelInfo Model Dto Servlet

--- src/com/flightscheduler/JavaEE/model/TravelInfo.java
+++ src/com/flightscheduler/JavaEE/model/TravelInfo.java
@@ -1,5 +1,97 @@
 package com.flightscheduler.JavaEE.model;
 
+import java.util.Date;
+
 public class TravelInfo {
+	private String tripType;
+	private int noOfAdults;
+	private int noOfChildren;
+	private String departurePlace;
+	private Date departureDate;
+	private Date returnDate;
+	private String returnPlace;
+	private Long id;
+
+	public TravelInfo() {
+		super();
+	}
+	
+	public TravelInfo(String tripType, int noOfAdults, int noOfChildren, String departurePlace, Date departureDate,
+			Date returnDate, String returnPlace) {
+		super();
+		this.tripType = tripType;
+		this.noOfAdults = noOfAdults;
+		this.noOfChildren = noOfChildren;
+		this.departurePlace = departurePlace;
+		this.departureDate = departureDate;
+		this.returnDate = returnDate;
+		this.returnPlace = returnPlace;
+	}
+	
+	
+	
+	public String getTripType() {
+		return tripType;
+	}
+
+	public void setTripType(String tripType) {
+		this.tripType = tripType;
+	}
+
+	public int getNoOfAdults() {
+		return noOfAdults;
+	}
+
+	public void setNoOfAdults(int noOfAdults) {
+		this.noOfAdults = noOfAdults;
+	}
+
+	public int getNoOfChildren() {
+		return noOfChildren;
+	}
+
+	public void setNoOfChildren(int noOfChildren) {
+		this.noOfChildren = noOfChildren;
+	}
+
+	public String getDeparturePlace() {
+		return departurePlace;
+	}
+
+	public void setDeparturePlace(String departurePlace) {
+		this.departurePlace = departurePlace;
+	}
+
+	public Date getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(Date departureDate) {
+		this.departureDate = departureDate;
+	}
+
+	public Date getReturnDate() {
+		return returnDate;
+	}
+
+	public void setReturnDate(Date returnDate) {
+		this.returnDate = returnDate;
+	}
+
+	public String getReturnPlace() {
+		return returnPlace;
+	}
+
+	public void setReturnPlace(String returnPlace) {
+		this.returnPlace = returnPlace;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 }
--- src/com/flightscheduler/JavaEE/servlet/TravelInfoServlet.java
+++ src/com/flightscheduler/JavaEE/servlet/TravelInfoServlet.java
@@ -1,11 +1,17 @@
 package com.flightscheduler.JavaEE.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.flightscheduler.JavaEE.dto.TravelInfoDto;
+import com.flightscheduler.JavaEE.model.TravelInfo;
 
 /**
  * Servlet implementation class TravelInfoServlet
@@ -13,7 +19,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TravelInfoServlet")
 public class TravelInfoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+     TravelInfoDto travelInfoDto = new TravelInfoDto();
+     SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+     TravelInfo travelInfo = new TravelInfo();
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,8 +42,30 @@ public class TravelInfoServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
+		try {
+			travelInfoDto.setDepartureDate(formatter.parse(request.getParameter("departureDate")));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		travelInfoDto.setTripType(request.getParameter("tripType"));
+		travelInfoDto.setNoOfAdults(Integer.parseInt(request.getParameter("noOfAdults")));
+		travelInfoDto.setNoOfChildren(Integer.parseInt(request.getParameter("noOfChildren")));
+		travelInfoDto.setDeparturePlace(request.getParameter("departurePlace"));
+		travelInfoDto.setReturnPlace(request.getParameter("returnPlace"));
+		try {
+			travelInfoDto.setReturnDate(formatter.parse(request.getParameter("returnDate")));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
