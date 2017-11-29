e4707eca1777f6963db28f21bf65db0e904eebdd
Munal <dhakal.munal@gmail.com>
2017-11-29 22:13:19 +0545

    CardHolderDetailsDatabse Fixed

--- src/com/flightscheduler/JavaEE/database/CardHolderDetailsDatabase.java
+++ src/com/flightscheduler/JavaEE/database/CardHolderDetailsDatabase.java
@@ -8,12 +8,12 @@ public class CardHolderDetailsDatabase {
 	public static Connection createCardHolderDetailsDatabase() {
 		System.out.println("Hello0");
 		Connection cn = CreateDatabase.createDatabase();
-		System.out.println("Hello0.1");
+		System.out.println("Hello : "+cn);
 		try {
 			Statement st = cn.createStatement();
-			System.out.println("Hello1");
-			String query = "create table if not exists cardholderdetails(id int(16) auto_increment,noOfAdults int(10),noOfChildrens int(10),chFirstName varchar(20),chLastName varchar(20),chMiddleName varchar(20),phoneNumber bigint(15),emailAddress varchar(40),pid Long,fid Long,PRIMARY KEY (id),FOREIGN KEY(fid) REFERENCES travelinfo(id),FOREIGN KEY(pid) REFERENCES passengerdetails(id)";
-			System.out.println("Hello2");
+			System.out.println("Hello :"+st);
+			String query = "create table if not exists cardholderdetails(id int(16) auto_increment,noOfAdults int(10),noOfChildrens int(10),chFirstName varchar(20),chLastName varchar(20),chMiddleName varchar(20),phoneNumber bigint(15),emailAddress varchar(40),pid int(16),fid int(16),PRIMARY KEY (id),FOREIGN KEY(fid) REFERENCES travelinfo(id),FOREIGN KEY(pid) REFERENCES passengerdetails(id))";
+			System.out.println("Hello :"+query);
 			st.executeUpdate(query);
 			System.out.println("Hello3");
 		} catch (SQLException e) {
